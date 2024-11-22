cb67293d62acc9c031a3ab984bbb1e465fde565d
Jose Gregorio Romero Mendoza <jgromerom89@gmail.com>
2024-11-21 23:16:29 -0300

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     screens: {
@@ -31,4 +33,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+}

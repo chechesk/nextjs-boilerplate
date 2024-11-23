02992574e94780c71a52677c1804900dad60e00c
Jose Gregorio Romero Mendoza <jgromerom89@gmail.com>
2024-11-22 22:52:26 -0300

    Update requeriment.tsx

--- Components/Config/requeriment.tsx
+++ Components/Config/requeriment.tsx
@@ -8,7 +8,7 @@ import { InstanceApi } from './api';
 const fetchPokemons = async (): Promise<Pokemon[]> => {
   try {
     const response: AxiosResponse<PokemonApiResponse> = await InstanceApi.get(
-      "pokemon?limit=1300"
+      "pokemon?limit=500"
     );
     const pokemonList = response.data.results;
 
